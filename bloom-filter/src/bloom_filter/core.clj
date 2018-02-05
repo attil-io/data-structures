50d16197fda7ae685852eebefb03119e79ee5518
Attilio <parrotchaffinch@gmail.com>
2018-02-05 22:50:05 +0100

    Empty bloom-create.

--- bloom-filter/src/bloom_filter/core.clj
+++ bloom-filter/src/bloom_filter/core.clj
@@ -1,7 +1,4 @@
-(ns bloom-filter.core
-  (:gen-class))
+(ns bloom-filter.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn bloom-create [numbits hashes])
--- bloom-filter/test/bloom_filter/core_test.clj
+++ bloom-filter/test/bloom_filter/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [bloom-filter.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest create-test
+  (testing "create bloom filter"
+    (is (= nil (bloom-create nil nil)))))
