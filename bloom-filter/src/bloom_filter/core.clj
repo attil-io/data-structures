ca12755bc49f04227e2d5695272972800b387674
Attilio <parrotchaffinch@gmail.com>
2018-02-07 05:42:10 +0100

    Add to bloom filter.

--- bloom-filter/src/bloom_filter/core.clj
+++ bloom-filter/src/bloom_filter/core.clj
@@ -3,4 +3,11 @@
 
 (defn bloom-create [numbits hash-functions]
       (if (or (nil? numbits) (nil? hash-functions)) nil
-          {:bits (repeat numbits 0) :hash-functions hash-functions}))
+          {:bits (vec (repeat numbits 0)) :hash-functions hash-functions}))
+
+(defn bloom-add [bloom-filter value]
+      (let [hash-functions (:hash-functions bloom-filter)
+            bits           (:bits bloom-filter)
+            new-bits (reduce (fn [actual-bits hash-function] (assoc actual-bits (hash-function value) 1)) bits hash-functions)]
+      (assoc-in bloom-filter [:bits] new-bits)))
+
--- bloom-filter/test/bloom_filter/core_test.clj
+++ bloom-filter/test/bloom_filter/core_test.clj
@@ -1,4 +1,5 @@
 (ns bloom-filter.core-test
+
   (:require [clojure.test :refer :all]
             [bloom-filter.core :refer :all]))
 
@@ -11,5 +12,13 @@
     (is (= {:bits [] :hash-functions []} (bloom-create 0 [])))
     (is (= {:bits [0] :hash-functions []} (bloom-create 1 [])))
     (is (= {:bits [] :hash-functions [always-zero]} (bloom-create 0 [always-zero])))
+)))
+
+(deftest add-test
+  (let [mod7fun (fn [num] (mod num 7))
+        empty-filter (bloom-create 7 [mod7fun])]
+  (testing "add to bloom filter"
+    (is (= {:bits [0 0 0 1 0 0 0] :hash-functions [mod7fun]}
+           (bloom-add empty-filter 3)))
 )))
 
