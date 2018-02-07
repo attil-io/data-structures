5b83ee1975ca7318a01c05366282f2de4d102220
Attilio <parrotchaffinch@gmail.com>
2018-02-07 20:34:14 +0100

    Refactor test cases.

--- bloom-filter/test/bloom_filter/core_test.clj
+++ bloom-filter/test/bloom_filter/core_test.clj
@@ -1,32 +1,32 @@
 (ns bloom-filter.core-test
 
-  (:require [clojure.test :refer :all]
-            [bloom-filter.core :refer :all]))
+(:require [clojure.test :refer :all]
+          [bloom-filter.core :refer :all]))
+
+(defn mod7-fun [num] (mod num 7))
+(defn always-zero-fun [dontcare] 0)  
 
 (deftest create-test
-  (letfn [(always-zero [dontcare] 0)]
   (testing "create bloom filter"
     (is (= nil (bloom-create nil nil)))
     (is (= nil (bloom-create 0 nil)))
     (is (= nil (bloom-create nil [])))
     (is (= {:bits [] :hash-functions []} (bloom-create 0 [])))
     (is (= {:bits [0] :hash-functions []} (bloom-create 1 [])))
-    (is (= {:bits [] :hash-functions [always-zero]} (bloom-create 0 [always-zero])))
-)))
+    (is (= {:bits [] :hash-functions [always-zero-fun]} (bloom-create 0 [always-zero-fun])))
+))
 
 (deftest add-test
-  (let [mod7fun (fn [num] (mod num 7))
-        alwaysonefun (fn [num] 1)
-        empty-filter (bloom-create 7 [])
-        single-fun-filter (bloom-create 7 [mod7fun])
-        two-fun-filter (bloom-create 7 [mod7fun alwaysonefun])]
+  (let [empty-filter (bloom-create 7 [])
+        single-fun-filter (bloom-create 7 [mod7-fun])
+        two-fun-filter (bloom-create 7 [mod7-fun always-zero-fun])]
   (testing "add to bloom filter"
     (is (= nil (bloom-add nil 3)))
     (is (= empty-filter (bloom-add empty-filter nil)))
     (is (= empty-filter (bloom-add empty-filter 10)))
-    (is (= {:bits [0 0 0 1 0 0 0] :hash-functions [mod7fun]}
+    (is (= {:bits [0 0 0 1 0 0 0] :hash-functions [mod7-fun]}
            (bloom-add single-fun-filter 3)))
-    (is (= {:bits [0 1 0 1 0 0 0] :hash-functions [mod7fun alwaysonefun]}
+    (is (= {:bits [1 0 0 1 0 0 0] :hash-functions [mod7-fun always-zero-fun]}
            (bloom-add two-fun-filter 3)))
 )))
 
