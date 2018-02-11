7b49358d0064e909b1f559e3ede74ab20d79fa98
Attilio <parrotchaffinch@gmail.com>
2018-02-11 21:49:30 +0100

    reduce over transient vector in bloom-add

--- bloom-filter/src/bloom_filter/core.clj
+++ bloom-filter/src/bloom_filter/core.clj
@@ -5,7 +5,7 @@
       (assert (number? numbits) "numbits must be numeric")
       (assert (not (nil? hash-functions)) "hash-functions must not be nil")
       (assert (every? ifn? hash-functions) "hash-functions must be a collection of functions")
-      {:bits (apply vector-of :boolean (repeat numbits false)) 
+      {:bits (vec (repeat numbits false)) 
        :hash-functions hash-functions})
 
 (def ^:private safe-hash-functions (memoize 
@@ -16,9 +16,9 @@
       (when bloom-filter 
       (let [bits (:bits bloom-filter)
             hash-functions (safe-hash-functions (count bits) (:hash-functions bloom-filter))]
-;            new-bits (reduce (fn [actual-bits hash-function] (assoc actual-bits (hash-function value) true)) bits hash-functions)]
       (->> hash-functions
-           (reduce (fn [actual-bits hash-function] (assoc actual-bits (hash-function value) true)) bits)
+           (reduce (fn [actual-bits hash-function] (assoc! actual-bits (hash-function value) true)) (transient bits))
+           (persistent!)
            (assoc bloom-filter :bits)))))
 
 (defn bloom-contains [bloom-filter value] 
