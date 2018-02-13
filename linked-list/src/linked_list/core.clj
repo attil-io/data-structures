28f5863261ed661f3f2440ee5804fdfd50ef507e
Attilio <parrotchaffinch@gmail.com>
2018-02-13 21:46:04 +0100

    dummy add-to-linked-list

--- linked-list/src/linked_list/core.clj
+++ linked-list/src/linked_list/core.clj
@@ -1,3 +1,5 @@
 (ns linked-list.core)
 
-(defn create-linked-list [] )
+(defn create-linked-list [] {})
+
+(defn add-to-linked-list [linked-list value] {:value value :next nil})
--- linked-list/test/linked_list/core_test.clj
+++ linked-list/test/linked_list/core_test.clj
@@ -5,3 +5,9 @@
 (deftest create-linked-list-test
   (testing "create linked list"
     (is (= {} (create-linked-list)))))
+
+(deftest add-to-linked-list-test
+  (let [empty-linked-list (create-linked-list)]
+  (testing "add to linked list"
+    (is (= {:value 10 :next nil} (add-to-linked-list empty-linked-list 10))))))
+
