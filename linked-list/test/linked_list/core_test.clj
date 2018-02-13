3dec262688d4fa3f58da17e705fe239c4cc697ed
Attilio <parrotchaffinch@gmail.com>
2018-02-13 21:59:59 +0100

    rename next to next-node (to not clash with keyword)

--- linked-list/test/linked_list/core_test.clj
+++ linked-list/test/linked_list/core_test.clj
@@ -9,6 +9,6 @@
 (deftest add-to-linked-list-test
   (let [empty-linked-list (create-linked-list)]
   (testing "add to linked list"
-    (is (= {:value 10 :next nil} (add-to-linked-list empty-linked-list 10)))
-    (is (= {:value 10 :next {:value 20 :next nil}} (add-to-linked-list (add-to-linked-list empty-linked-list 10) 20))))))
+    (is (= {:value 10 :next-node nil} (add-to-linked-list empty-linked-list 10)))
+    (is (= {:value 10 :next-node {:value 20 :next-node nil}} (add-to-linked-list (add-to-linked-list empty-linked-list 10) 20))))))
 
