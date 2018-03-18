d66b1d105be784ac09f109da94b227c5966d1026
Attilio <parrotchaffinch@gmail.com>
2018-03-18 15:22:54 +0100

    Refactor tests

--- linked-list/test/linked_list/core_test.clj
+++ linked-list/test/linked_list/core_test.clj
@@ -10,25 +10,31 @@
   ([value] (new-node value nil))
   ([value next-node] (->Node value next-node)))
 
-(def empty-linked-list (create-linked-list))
+(defn new-linked-list [& values] 
+  (loop [value (last values)
+         other-values (reverse values)
+         node nil]
+  (if (empty? other-values) node
+      (recur (first (rest other-values)) (rest other-values) (new-node value node)))))
+
+(def empty-linked-list (new-linked-list))
+(def one-element-linked-list (new-linked-list 10))
+(def two-element-linked-list (new-linked-list 10 20))
+(def three-element-linked-list (new-linked-list 10 20 30))
+
 
 (deftest add-to-linked-list-test
   (testing "add to linked list"
-    (is (= (new-node 10) (add-to-linked-list empty-linked-list 10)))
-    (is (= (new-node 10 (new-node 20)) (add-to-linked-list (add-to-linked-list empty-linked-list 10) 20)))
-    (is (= (new-node 10 (new-node 20 (new-node 30))) (add-to-linked-list (add-to-linked-list (add-to-linked-list empty-linked-list 10) 20) 30)))))
-
-(def one-element-linked-list (add-to-linked-list empty-linked-list 10))
-(def two-element-linked-list (add-to-linked-list one-element-linked-list 20))
+    (is (= one-element-linked-list (add-to-linked-list empty-linked-list 10)))
+    (is (= two-element-linked-list (add-to-linked-list (add-to-linked-list empty-linked-list 10) 20)))
+    (is (= three-element-linked-list (add-to-linked-list (add-to-linked-list (add-to-linked-list empty-linked-list 10) 20) 30)))))
 
 (deftest contains-linked-list-test
-  (let [one-element-linked-list (add-to-linked-list empty-linked-list 10)
-        two-element-linked-list (add-to-linked-list one-element-linked-list 20)]
     (testing "does a linked list contain a value"
       (is (false? (contains-linked-list? empty-linked-list 10)))
       (is (true?  (contains-linked-list? one-element-linked-list 10)))
       (is (true?  (contains-linked-list? two-element-linked-list 20)))
-      (is (false?  (contains-linked-list? two-element-linked-list 30))))))
+      (is (false?  (contains-linked-list? two-element-linked-list 30)))))
 
 (deftest get-nth-linked-list-test
   (testing "get the nth element of linked list"
@@ -36,14 +42,13 @@
     (is (= 10 (get-nth-linked-list one-element-linked-list 0)))
     (is (= 20 (get-nth-linked-list two-element-linked-list 1)))))
 
-(def three-element-linked-list (add-to-linked-list two-element-linked-list 30))
 
 (deftest without-element-linked-list-test
   (testing "remove the nth element of linked list"
     (is (= empty-linked-list (without-element-linked-list empty-linked-list 0)))
     (is (= empty-linked-list (without-element-linked-list one-element-linked-list 0)))
-    (is (= (new-node 20) (without-element-linked-list two-element-linked-list 0)))
+    (is (= (new-linked-list 20) (without-element-linked-list two-element-linked-list 0)))
     (is (= one-element-linked-list (without-element-linked-list two-element-linked-list 1)))
-    (is (= (new-node 10 (new-node 30)) (without-element-linked-list three-element-linked-list 1)))))
+    (is (= (new-linked-list 10 30) (without-element-linked-list three-element-linked-list 1)))))
 
  
